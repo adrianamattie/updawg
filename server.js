c84e5d4c454318ff4ca9c5b6a33cdfd45058e2a3
Adriana Shulman <adriana33@mac.com>
2018-07-28 11:37:41 -0400

    commiting my changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+require("dotenv").config();
 
 var db = require("./models");
 
