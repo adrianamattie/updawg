b5882c750994ade5bb389025f09a2519e702c374
Ananya Bala <ananyabala42@gmail.com>
2018-08-02 19:16:34 -0400

    commit my changes

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("slipIntoTheDB", "root", "ab429630", {
+var sequelize = new Sequelize("slipIntoThe_db", "root", "ab429630", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
