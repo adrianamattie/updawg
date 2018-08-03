952cd5f8f6a7dd54a5f8fef0747e59e9e87fc704
Ananya Bala <ananyabala42@gmail.com>
2018-08-02 21:24:33 -0400

    old version

--- app/public/js/logic.js
+++ app/public/js/logic.js
@@ -1,28 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  
 
-    $(".buddy").on("swiperight",function(){
-      $(this).addClass('rotate-left').delay(700).fadeOut(1);
-      $('.buddy').find('.status').remove();
+  $(".buddy").on("swiperight", function () {
+    $(this).addClass('rotate-left').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
 
-      $(this).append('<div class="status like">Like!</div>');      
-      if ( $(this).is(':last-child') ) {
-        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-       } else {
-          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-       }
-    });  
+    $(this).append('<div class="status like">Like!</div>');
+    if ($(this).is(':last-child')) {
+      $('.buddy:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
+    } else {
+      $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+  });
 
-   $(".buddy").on("swipeleft",function(){
+  $(".buddy").on("swipeleft", function () {
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
     $(this).append('<div class="status dislike">Dislike!</div>');
 
-    if ( $(this).is(':last-child') ) {
-     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+    if ($(this).is(':last-child')) {
+      $('.buddy:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
       alert('OUPS');
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-    } 
+    } else {
+      $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
   });
 
 });
--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,5 +1,6 @@
 
 $(document).ready(() => {
+    $('.modal').modal();
 
     $('.dropdown-trigger').dropdown();
 
@@ -117,51 +118,51 @@ var config = {
     projectId: "updawg-e1145",
     storageBucket: "updawg-e1145.appspot.com",
     messagingSenderId: "23565913569"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 
 
-  // Initialize the FirebaseUI Widget using Firebase.
+// Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start('#firebaseui-auth-container', {
     signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     // Other config options...
-  });
+});
 
 var uiConfig = {
     callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
     signInSuccessUrl: '/add',
     signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-      
+        // Leave the lines as is for the providers you want to offer your users.
+
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+
     ],
     // Terms of service url.
     tosUrl: '<your-tos-url>',
     // Privacy policy url.
     privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
+};
 
-  // The start method will wait until the DOM is loaded.
+// The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
 
 //get users uid on sign in
